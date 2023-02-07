c9205f1a58e5fdf28336888d5fe44f81f3dae9d5
arrtee38 <arrtee38@gmail.com>
2023-02-07 03:15:39 +0000

    linked backend to frontend

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,11 +1,7 @@
 class Feedback < ApplicationRecord
-    #psuedo-code
-    #1. user creates a feedback record
-    #2. tweet the "text" attribute of the feedback,to the "recipient_handle" attribute
-
     after_create :send_tweet
 
     def send_tweet 
-        puts "Sending tweet!"
+        TwitterService.tweet!(text)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/terms'
-  get 'pages/privacy'
+  # get 'pages/terms'
+  # get 'pages/privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#home'
+  post 'feedbacks', to: 'feedbacks#create'
+  
 end
